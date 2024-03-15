978f29acdfaf5d3df4af860a769c13d728d88a31
Francis <francis@cae.software>
2024-03-14 23:04:27 -0700

    added security/model access xml file

--- src/user/my_module/__manifest__.py
+++ src/user/my_module/__manifest__.py
@@ -2,13 +2,15 @@
     'name': 'Slack Odoo Connector',
     'version': '1.0',
     'summary': 'A slack connector and sync tool to take your Odoo record communication to the next level.',
-    'sequence': -100,
+    'sequence': 10,
     'description': """A slack connector .""",
     'category': 'Productivity',
     'website': 'https://www.procloud.consulting',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir_model_access.xml', #This line references your security file # other data files like views
+    ],
     'demo': [],
     'installable': True,
     'application': True,
